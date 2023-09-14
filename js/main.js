ba532f6f6d6c82deb6dd871fbf9f96ee3279f35c
aniruddha <aniruddhathakur76@gmail.com>
2023-09-14 23:45:45 +0530

    Task 1 Complete

---
+++ js/main.js
@@ -0,0 +1,9 @@
+gsap.from('.bottom_shoes_card', {opacity:0, delay:2, duration:3, ease: "elastic.out(1,0.3)", x:100});
+
+gsap.from('.selected_product_img', {opacity:0, delay:2, duration:3, ease:"elastic.out(1,0.3)", y:100});
+
+gsap.from('.size_product', {opacity:0, delay:3.5, duration:0.5, ease:"Expo.In", y:100});
+
+gsap.from('.circle1', {opacity:0, delay:4.5, scale:.1, duration:.7, ease:"expo.In"});
+
+gsap.from('.circle2', {opacity:0, delay:4.8, scale:.1, duration:.7, ease:"expo.In"});
